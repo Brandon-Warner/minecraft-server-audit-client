41a423e81984e9263c375f3993145e76cc64b03e
Brandon Warner <brandonswarner5@gmail.com>
2021-10-18 15:49:00 -0700

    SingleSearch fetch is formatted to return mode data

--- src/components/SingleSearch.js
+++ src/components/SingleSearch.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchHelper from '../services/servers';
+// import { getData } from '../reducers/dataReducer';
+// import { useDispatch } from 'react-redux';
 
 const SingleSearch = () => {
     const [name, setName] = useState('');
-    console.log('single search name: ', name);
+    const [searchName, setSearchName] = useState('');
+
+    // const dispatch = useDispatch();
+    // console.log('single search name: ', searchName);
+
+    useEffect(() => {
+        fetchHelper
+            .fetchAllData(searchName)
+            .then(response => console.log(response))
+            .catch(e => console.log('error: ', e.message));
+    }, [searchName]);
+
+    const submit = e => {
+        e.preventDefault();
+
+        setSearchName(name);
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={submit}>
                 enter url:
-                <input
-                    type='string'
-                    onChange={({ target }) => setName(target.value)}
-                />
+                <input type='string' onChange={({ target }) => setName(target.value)} />
                 <button type='submit'>search</button>
             </form>
         </div>
--- src/services/servers.js
+++ src/services/servers.js
@@ -1,53 +1,91 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const baseUrl = 'https://fast-thicket-91718.herokuapp.com/api'
+const baseUrl = 'https://fast-thicket-91718.herokuapp.com/api';
 
-const isOnline = data => (data === true ? 'yes' : 'no')
+const isOnline = data => (data === true ? 'yes' : 'no');
 const isPlayersOnline = data => {
     if (data.online === false) {
-        return 'N/A'
+        return 'N/A';
     }
-    return data.players.online
-}
+    return data.players.online;
+};
 const isPlayersMax = data => {
     if (data.online === false) {
-        return 'N/A'
+        return 'N/A';
     }
-    return data.players.max
-}
-const isBlocked = data => (data === true ? 'yes' : 'no')
+    return data.players.max;
+};
+const isBlocked = data => (data === true ? 'yes' : 'no');
 
 const formatDate = d => {
     if (d !== null) {
-        let date = new Date(d)
-        let dd = date.getDate()
-        let mm = date.getMonth() + 1
-        let yyyy = date.getFullYear()
+        let date = new Date(d);
+        let dd = date.getDate();
+        let mm = date.getMonth() + 1;
+        let yyyy = date.getFullYear();
         if (dd < 10) {
-            dd = '0' + dd
+            dd = '0' + dd;
         }
         if (mm < 10) {
-            mm = '0' + mm
+            mm = '0' + mm;
         }
-        return (d = mm + '/' + dd + '/' + yyyy)
+        return (d = mm + '/' + dd + '/' + yyyy);
     }
-    return 'N/A'
-}
+    return 'N/A';
+};
+
+const onlineInfoResponse = data => {
+    if (!data.success) {
+        return 'Error while checking offline-mode status';
+    } else if (data.offlineMode) {
+        return 'Server is using offline mode';
+    } else {
+        return "Server aborted the join process, it's either using online mode or a whitelist";
+
+        // if (data.reason) {
+        //     fetchResponse.concat(`reason was: ${response.reason}`);
+        // }
+    }
+};
 
 const fetchData = async name => {
-    console.log('fetchData name: ', name)
-    const server_data = await axios.get(`${baseUrl}/serverinfo/${name}`)
-    const serverInfo = server_data.data
+    console.log('fetchData name: ', name);
+    const server_data = await axios.get(`${baseUrl}/serverinfo/${name}`);
+    const serverInfo = server_data.data;
 
-    const block_data = await axios.get(`${baseUrl}/blockinfo/${name}`)
-    const blockInfo = block_data.data
+    const block_data = await axios.get(`${baseUrl}/blockinfo/${name}`);
+    const blockInfo = block_data.data;
 
-    // const offline_data = await axios.get(
-    //     `http://localhost:8080/api/offlineinfo/${name.name}`
-    // )
-    // const offlineInfo = offline_data.data
+    console.log('fetch data: ', serverInfo, blockInfo);
 
-    console.log('fetch data: ', serverInfo, blockInfo)
+    const allData = {
+        name: name,
+        hostname: serverInfo.hostname,
+        ip: serverInfo.ip,
+        version: serverInfo.version || 'N/A',
+        active: isOnline(serverInfo.online),
+        playersOnline: isPlayersOnline(serverInfo),
+        playersMax: isPlayersMax(serverInfo),
+        blocked: isBlocked(blockInfo.blocked),
+        blockTime: formatDate(blockInfo.lastBlocked)
+    };
+    console.log('allData: ', allData);
+    return allData;
+};
+
+const fetchAllData = async name => {
+    const server_data = await axios.get(`${baseUrl}/serverinfo/${name}`);
+    const serverInfo = server_data.data;
+
+    const block_data = await axios.get(`${baseUrl}/blockinfo/${name}`);
+    const blockInfo = block_data.data;
+
+    const offline_data = await axios.get(`${baseUrl}/offlineinfo/${name}`);
+    const offlineInfo = offline_data.data;
+
+    console.log('fetch all data SERVER INFO: ', serverInfo);
+    console.log('fetch all data BLOCK INFO: ', blockInfo);
+    console.log('fetch all data ONLINE INFO: ', offlineInfo);
 
     const allData = {
         name: name,
@@ -59,10 +97,11 @@ const fetchData = async name => {
         playersMax: isPlayersMax(serverInfo),
         blocked: isBlocked(blockInfo.blocked),
         blockTime: formatDate(blockInfo.lastBlocked),
-    }
-    console.log('allData: ', allData)
-    return allData
-}
+        modeData: onlineInfoResponse(offlineInfo)
+    };
+    console.log('allData: ', allData);
+    return allData;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { fetchData }
+export default { fetchData, fetchAllData };
